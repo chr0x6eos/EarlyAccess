1184fc703904279e67a75511af718a39a88b26e1
chr0x6eos <posssi01@gmail.com>
2021-04-16 21:46:34 +0200

    Delete mysql-storage

--- xss_automation/trigger_xss.py
+++ xss_automation/trigger_xss.py
@@ -6,11 +6,13 @@ from selenium.webdriver.chrome.options import Options
 class XSS:
     host = "https://earlyaccess.htb"
     driver = None
-    _password = "P@ssw0rd" # Default value
+    _password = "" # Default value
 
     def __init__(self):
-        self.driver = webdriver.Chrome('./chromedriver', options=self._set_chrome_options())
+        self.driver = webdriver.Chrome('/usr/bin/chromedriver', options=self._set_chrome_options())
         self._password = config("ADMIN_PW")
+        if self._password == "":
+            raise Exception("No password for admin configured!")
 
     def _set_chrome_options(self):
         """Sets chrome options for Selenium.
@@ -26,20 +28,25 @@ class XSS:
         chrome_prefs["profile.default_content_settings"] = {"images": 2}
         return chrome_options
 
-    def login(self):
-        self.driver.get(f"{self.host}/login")
-        self.driver.find_element_by_name("email").send_keys("admin@earlyaccess.htb")
-        self.driver.find_element_by_name("password").send_keys(self._password)
-        self.driver.find_element_by_tag_name("button").click()
-        
-        return self.driver.current_url
-        if self.driver.current_url != f"{self.host}/dashboard":
-            self.login() # retry
-        
-        return self.driver.get_cookies()
-        
-
+    def login(self, tries:int=0):
+        try:
+            # Max retries exceeded
+            if tries > 3:
+                raise Exception("Could not login!")
             
+            self.driver.get(f"{self.host}/login")
+            self.driver.find_element_by_name("email").send_keys("admin@earlyaccess.htb")
+            self.driver.find_element_by_name("password").send_keys(self._password)
+            self.driver.find_element_by_tag_name("button").click()
+            
+            return self.driver.current_url
+            if self.driver.current_url != f"{self.host}/dashboard":
+                tries = tries +1
+                self.login(tries) # retry
+            
+            return self.driver.get_cookies()
+        except Exception as ex:
+            return ex
 
     def req(self, url:str):
         return self.driver.get(url)
