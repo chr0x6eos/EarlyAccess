d76ffdc3b8ef735bdc46186d9a4d8e58c0092113
root <root@localhost.localdomain>
2021-04-26 20:38:34 +0200

    Add iptables examples

---
+++ scripts/mysql/setup/mysql.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Create game-user with READ-ONLY permission on the users table and write permissions on
+# Set sql_mode to allow group by query 
+mysql --protocol=socket -uroot -p$MYSQL_ROOT_PASSWORD <<EOSQL
+CREATE USER "game"@"%" IDENTIFIED BY "$GAME_PW";
+CREATE USER "dev"@"%" IDENTIFIED BY "$DEV_PW";
+GRANT SELECT ON $MYSQL_DATABASE.users TO "game"@"%","dev"@"%";
+GRANT SELECT,INSERT ON $MYSQL_DATABASE.scoreboard TO "game"@"%";
+FLUSH PRIVILEGES;
+EOSQL
---
+++ server-conf/README.md
@@ -0,0 +1,7 @@
+# Server configuration
+
+## iptables
+```bash
+apt-get install iptables-persistent
+iptables-save > /etc/iptables/rules.v4
+```
